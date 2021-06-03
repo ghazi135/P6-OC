61b78ed6bab15b409741beb053f6496350782203
GBOUZAZI <Ghazi.BOUZAZI@hardis-group.com>
2021-06-04 01:17:00 +0200

    transactions commit rollback

--- src/main/java/com/openclassrooms/paymybuddy/service/TransactionService.java
+++ src/main/java/com/openclassrooms/paymybuddy/service/TransactionService.java
@@ -7,11 +7,13 @@ import com.openclassrooms.paymybuddy.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Service
+@Transactional
 public class TransactionService {
 
     @Autowired
