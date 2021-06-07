dc3c4be92adf8a4a67118eca2f73067316512548
GBOUZAZI <Ghazi.BOUZAZI@hardis-group.com>
2021-06-08 00:49:41 +0200

    Integration testing TransactionService

--- src/test/java/com/openclassrooms/paymybuddy/it/TransactionServiceIT.java
+++ src/test/java/com/openclassrooms/paymybuddy/it/TransactionServiceIT.java
@@ -1,6 +1,9 @@
 package com.openclassrooms.paymybuddy.it;
 
 
+import com.openclassrooms.paymybuddy.entity.BankAccount;
+import com.openclassrooms.paymybuddy.entity.Transaction;
+import com.openclassrooms.paymybuddy.entity.User;
 import com.openclassrooms.paymybuddy.repository.AccountRepository;
 import com.openclassrooms.paymybuddy.repository.TransactionRepository;
 import com.openclassrooms.paymybuddy.repository.UserRepository;
@@ -13,6 +16,11 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 
 @DataJpaTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -40,4 +48,61 @@ public class TransactionServiceIT {
         Assertions.assertNotNull(testEntityManager);
     }
 
+    @Test
+    void findAllTransactions() {
+
+        Transaction       transaction     = new Transaction(1, "ghgg", 111.00, null, null);
+        transactionRepository.save(transaction);
+        Assertions.assertFalse(transactionService.findAllTransactions().isEmpty());
+    }
+
+    @Test
+    void findAllTransactionByPrincipalUser() {
+
+        User              user            = new User(1, "aaaa", "bbbb", "ggg@gggg", "111gg", 111.00, null, null);
+        Transaction       transaction     = new Transaction(1, "ghgg", 111.00,null,user);
+        user.setBankAccount(new BankAccount());
+        userRepository.save(user);
+        transactionRepository.save(transaction);
+        Assertions.assertFalse(transactionService.findTransactionsOfPrincipalUser(user).isEmpty());
+    }
+
+
+    @Test
+    void sendMoneyTest() {
+
+        User user  = new User(1, "aaaa", "bbbb", "ggg@gggg", "111gg", 10000.00 + (10000.00 * 0.005), null, null);
+        User user1 = new User(2, "aaazzza", "bbbzzzb", "ggzzzg@gggg", "111zgg", 0.00, null, null);
+        userRepository.save(user);
+        userRepository.save(user1);
+
+        transactionService.sendMoney(user.getEmail(), 10000.00, user1.getEmail(), "");
+
+        Assertions.assertEquals(userRepository.findById(1).get().getMoneyAvailable(), 0.00);
+        Assertions.assertEquals(userRepository.findById(2).get().getMoneyAvailable(), 10000.00);
+
+    }
+
+    @Test
+    void getBackMoneyInMyBankAccountTest() {
+
+        User user  = new User(1, "aaaa", "bbbb", "ggg@gggg", "111gg", 10000.00 , null, null);
+        userRepository.save(user);
+        transactionService.getBackMoneyOnMyBankAccount(user.getEmail(),10000.00);
+        Assertions.assertEquals(userRepository.findById(user.getId()).get().getMoneyAvailable(),0.00);
+
+    }
+
+    @Test
+    void rechargeMyBankAccountTest() {
+        BankAccount bankAccount = new BankAccount(1, "111111", "44444", new User(1, "aaaa", "bbbb", "ggg@gggg", "111gg", 0.00 , null, null));
+        User user  = new User(1, "aaaa", "bbbb", "ggg@gggg", "111gg", 0.00 , bankAccount, null);
+        user.setBankAccount(bankAccount);
+        bankAccount.setUser(user);
+        userRepository.save(user);
+        accountRepository.save(bankAccount);
+        transactionService.rechargeApplicationAccount(user.getEmail(),10000.00,user.getBankAccount().getIBAN());
+        Assertions.assertEquals(userRepository.findById(user.getId()).get().getMoneyAvailable(),10000.00);
+
+    }
 }
