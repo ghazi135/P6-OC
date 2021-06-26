ce1bb9d0646b91fdfe32af5c73a865556bcdc7eb
GBOUZAZI <Ghazi.BOUZAZI@hardis-group.com>
2021-06-26 14:00:20 +0200

    jeux de données sql

--- src/main/resources/sql-script.sql
+++ src/main/resources/sql-script.sql
@@ -118,5 +118,22 @@ VALUES ('blabla', 10, 3, 1);
 INSERT INTO `transaction`(`description`, `amount`, `beneficiary_user_id`, `payer_user_id`)
 VALUES ('blabla', 10, 3, 2);
 
+INSERT INTO `user_friends`(`user_id`, `user_friend_id`)
+VALUE (1,2);
+
+INSERT INTO `user_friends`(`user_id`, `user_friend_id`)
+    VALUE (2,1);
+
+INSERT INTO `user_friends`(`user_id`, `user_friend_id`)
+    VALUE (1,3);
+
+INSERT INTO `user_friends`(`user_id`, `user_friend_id`)
+    VALUE (3,1);
+
+INSERT INTO `user_friends`(`user_id`, `user_friend_id`)
+    VALUE (2,3);
+
+INSERT INTO `user_friends`(`user_id`, `user_friend_id`)
+    VALUE (3,2);
 
 
